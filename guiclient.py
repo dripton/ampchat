eb77e95009c51c2844a2e101ac4a902a991a68ef
David Ripton <david.ripton@asti-usa.com>
2007-10-25 14:19:39 -0400

    Don't make a new connection if we already have one. Save protocol in self.protocol

--- guiclient.py
+++ guiclient.py
@@ -9,6 +9,7 @@ import gtk
 
 from diceserver import RollDice, port
 
+host = "localhost"
 
 class GUIClient(gtk.Window):
     def __init__(self):
@@ -28,6 +29,7 @@ class GUIClient(gtk.Window):
         self.image.show()
         self.vbox.pack_start(self.image)
         self.show()
+        self.protocol = None
 
     def done(self, result):
         self.vbox.remove(self.image)
@@ -38,15 +40,18 @@ class GUIClient(gtk.Window):
         self.image.show()
 
     def connect_finished(self, protocol):
+        self.protocol = protocol
         d1 = protocol.callRemote(RollDice, sides=6)
         d1.addCallback(lambda result_dict: result_dict['result'])
         d1.addCallback(self.done)
 
     def roll(self, widget, event):
-        host = "localhost"
-        clientcreator = ClientCreator(reactor, amp.AMP)
-        d1 = clientcreator.connectTCP(host, port)
-        d1.addCallback(self.connect_finished)
+        if self.protocol is None:
+            clientcreator = ClientCreator(reactor, amp.AMP)
+            d1 = clientcreator.connectTCP(host, port)
+            d1.addCallback(self.connect_finished)
+        else:
+            self.connect_finished(self.protocol)
 
     def stop(self, unused):
         reactor.stop()
