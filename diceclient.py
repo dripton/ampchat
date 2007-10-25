1c93284b8aa61628fcb7622c424af9600d810444
David Ripton <david.ripton@asti-usa.com>
2007-10-25 14:46:03 -0400

    Command line args for host and port Refactoring for readability

--- diceclient.py
+++ diceclient.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python
 
+import sys
+
 from twisted.internet import reactor, defer
 from twisted.internet.protocol import ClientCreator
 from twisted.protocols import amp
+from twisted.python import usage
+
 from diceserver import RollDice, port
 
 
-def roll_die():
-    host = "localhost"
-    d1 = ClientCreator(reactor, amp.AMP).connectTCP(
-        host, port).addCallback(
-            lambda p: p.callRemote(RollDice, sides=6)).addCallback(
-                lambda result: result['result'])
+class Options(usage.Options):
+        optParameters = [
+            ["host", "h", "localhost", "server hostname"],
+            ["port", "p", port, "server port"],
+        ]
+
+
+def roll_die(host, port):
+    clientcreator = ClientCreator(reactor, amp.AMP)
+    d1 = clientcreator.connectTCP(host, port)
+    d1.addCallback(lambda p: p.callRemote(RollDice, sides=6))
+    d1.addCallback(lambda result: result['result'])
     def done(result):
         print 'Got roll:', result
         reactor.stop()
     d1.addCallback(done)
+    d1.addErrback(failure)
+
+def failure(error):
+    print "failed", str(error)
+    reactor.stop()
 
 if __name__ == '__main__':
-    roll_die()
+    options = Options()
+    try:
+        options.parseOptions()
+    except usage.UsageError, err:
+        print "%s: %s" % (sys.argv[0], err)
+        print "%s: Try --help for usage details" % sys.argv[0]
+        sys.exit(1)
+
+    host = options["host"]
+    port = int(options["port"])
+    roll_die(host, port)
     reactor.run()
