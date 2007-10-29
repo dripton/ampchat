d86b0c05136278814c9339b8339ba4043aba61fd
David Ripton <david.ripton@asti-usa.com>
2007-10-29 13:48:48 -0400

    Ensure that hostname and port are valid before calling callback.

--- connect.py
+++ connect.py
@@ -18,12 +18,41 @@ class ConnectDialog(object):
     def response(self, dialog, response_id):
         if response_id == gtk.RESPONSE_OK:
             hostname = self.hostname_entry.get_text()
-            port = self.port_entry.get_text()
-            self.callback(hostname, port)
+            if not hostname:
+                messagedialog = gtk.MessageDialog(parent=self.connect_dialog,
+                  type=gtk.MESSAGE_ERROR,
+                  buttons=gtk.BUTTONS_OK, 
+                  message_format="Need a hostname")
+                messagedialog.run()
+                messagedialog.destroy()
+                return True
+            port_str = self.port_entry.get_text()
+            try:
+                port = int(port_str)
+            except ValueError:
+                messagedialog = gtk.MessageDialog(parent=self.connect_dialog,
+                  type=gtk.MESSAGE_ERROR,
+                  buttons=gtk.BUTTONS_OK, 
+                  message_format="Port must be a number")
+                messagedialog.run()
+                messagedialog.destroy()
+                return True
+            else:
+                if port < 1 or port > 65535:
+                    messagedialog = gtk.MessageDialog(parent=self.connect_dialog,
+                      type=gtk.MESSAGE_ERROR,
+                      buttons=gtk.BUTTONS_OK,
+                      message_format="Port must be in range 1-65535")
+                    messagedialog.run()
+                    messagedialog.destroy()
+                    return True
+        self.callback(hostname, port)
+        self.connect_dialog.destroy()
     
 
 if __name__ == "__main__":
     def print_callback(hostname, port):
         print("hostname: %s port: %s" % (hostname, port))
+        gtk.main_quit()
     connect_dialog = ConnectDialog(print_callback)
-    response = connect_dialog.connect_dialog.run()
+    gtk.main()
